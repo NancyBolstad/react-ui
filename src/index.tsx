cb087b42dad7d34ce6e641ffdf1b13f28a477919
NancyBolstad <litchacha@gmail.com>
2019-08-24 22:05:45 +0200

    Test precommit hook

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const test=    6666;
+const lintTest=     99999;
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
